e0f937a075cdebee843287739e664c301703e090
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-13 16:27:32 -0300

    independents attributes

--- dist/CreateCourseService.js
+++ dist/CreateCourseService.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var createCourseService = /** @class */ (function () {
     function createCourseService() {
     }
-    createCourseService.prototype.execute = function (name, duration, educator) {
+    createCourseService.prototype.execute = function (_a) {
+        var duration = _a.duration, educator = _a.educator, name = _a.name;
         console.log(name, duration, educator);
     };
     return createCourseService;
--- dist/routes.js
+++ dist/routes.js
@@ -6,7 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCourse = void 0;
 var CreateCourseService_1 = __importDefault(require("./CreateCourseService"));
 function createCourse(request, response) {
-    CreateCourseService_1.default.execute("NodeJS", 10, "Lígia");
+    CreateCourseService_1.default.execute({
+        name: "NodeJS",
+        educator: "Lígia",
+        duration: 10
+    });
     return response.send();
 }
 exports.createCourse = createCourse;
